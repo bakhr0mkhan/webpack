6cc4a77e20ccbc1df393c1c8c22d9b1892e8c76d
bakhr0mkhan <softdeveloperuz@gmail.com>
2022-03-11 10:10:42 +0500

    SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   module: {
     rules: [
+        //js files
       {
         test: /\.?js$/,
         exclude: /node_modules/,
@@ -18,9 +19,22 @@ module.exports = {
           }
         }
       },
+      //css files
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      //scss | sass 
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ]
   },
