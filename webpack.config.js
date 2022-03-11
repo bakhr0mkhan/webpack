2492db9be81028e92b38f9220d630e4b0d213fe5
bakhr0mkhan <softdeveloperuz@gmail.com>
2022-03-11 16:04:12 +0500

    Image loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path:path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
-        //js files
+      //js files
       {
         test: /\.?js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
       },
       //css files
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      //scss | sass 
+      //scss | sass
       {
         test: /\.s[ac]ss$/i,
         use: [
@@ -36,11 +36,20 @@ module.exports = {
           "sass-loader",
         ],
       },
-    ]
+      //image
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html"),
     }),
   ],
-}
+};
